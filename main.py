db7b443cb74312120146d0719cd6833ea9d5e192
Ethan Fo <efox6@cse-sd-04.unl.edu>
2022-01-22 12:05:31 -0600

    Add user input for scales and amount generated

--- main.py
+++ main.py
@@ -1,24 +1,42 @@
 import random
 
+scale_dict = {
+  'C Major' : ['C', 'D', 'E', 'F', 'G', 'A', 'B', 'C^'],
+  'D Major' : ['D', 'E', 'F#', 'G', 'A', 'B', 'C#', 'D^'],
+  'E Major' : ['E', 'F#', 'G#', 'A', 'B', 'C#', 'D#', 'E^'],
+  'F Major' : ['F', 'G', 'A', 'b', 'C', 'D', 'E', 'F'],
+  'G Major' : ['G', 'A', 'B', 'C', 'D', 'E', 'F#', 'G^'],
+  'C Minor' : ['C', 'D', 'e', 'F', 'G', 'a', 'b', 'C^']
+}
+
+
 def main():
-  notes = ['A', 'A#', 'a', 'B', 'B#', 'b', 'C', 'C#', 'c', 'D', 'D#', 'd', 'E', 'E#', 'e', 'F', 'F#', 'f', 'G', 'G#', 'g']
-  modifiers = ['#', '_']
+  print('Standard notes are written as capitals. Sharps have a # appended. Flats are in lowercase. Ex: A, D#, e')
+  print('The app assumes 4/4 time. Beatss are separated by commas. A note being held for a beat uses a -. Ex: F--')
+  print('Eighth notes are written together in a beat and separated with a /. Ex: A/B, ')
+  print("The following scales are available:")
+  for key in scale_dict:
+    print(key, end=' ')
+  print()
+  scale_input = input("Enter the key to use: ")
+  scale = scale_dict[scale_input]
+  measures = input("Enter how many measures you would like to generate: ")
   out = ""
-  for i in range(16):
-    nextBeat = ''
-    noteType = random.randrange(0,100,1)
-    if (noteType < 50): # quater note
-      nextBeat = random.choice(notes)
-    elif (noteType < 70): # eighth note
-      nextBeat = random.choice(notes)
-      nextBeat += '/' + random.choice(notes)
-    elif (noteType < 85): # hold note
+  for i in range(int(measures) * 4):
+    next_beat = ''
+    note_type = random.randrange(0,100,1)
+    if (note_type < 50): # quater note
+      next_beat = random.choice(scale)
+    elif (note_type < 70): # eighth note
+      next_beat = random.choice(scale)
+      next_beat += '/' + random.choice(scale)
+    elif (note_type < 85): # hold note
       # change to delete the last character first
       out = out[:-1]
-      nextBeat = '-'
+      next_beat = '-'
     else:
-      nextBeat = ' '
-    out += nextBeat + ','
+      next_beat = ' '
+    out += next_beat + ','
   print()
   print(out)
   print()
